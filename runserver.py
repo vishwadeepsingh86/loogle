bb179b9c16b0a4b162d51bbe7e9fb007e99c05a1
Vishwadeep Singh <vsdpsingh@gmail.com>
2024-04-01 14:21:35 +0530

    Create runserver.py

---
+++ runserver.py
@@ -0,0 +1,21 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"Hello, World!")
+
+def run_server():
+    host = 'localhost'
+    port = 8000
+
+    server_address = (host, port)
+    httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    print(f"Server running on http://{host}:{port}")
+
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run_server()
